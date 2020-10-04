529fff9d79be208d8c9299ffa151bfe95d74d3b9
rachiddeka <chaidadeka@gmail.com>
2020-10-04 17:10:15 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        lettre = chr(ord(lettre))
+        lettre = chr(ord)
         resultat += lettre
     return resultat
 
